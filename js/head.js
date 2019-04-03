3daf42242318e92ab914c4f520ac8c2b15a2518c
ImmatureKid <1770125568@qq.com>
2019-04-03 20:21:32 +0800

    打点

--- js/head.js
+++ js/head.js
@@ -1,8 +1,8 @@
 $(function(){
 	$('.header-tap-ul li').click(function(){
-		// console.log(1)
+		console.log(1)
 		$(this).addClass("active").siblings().removeClass("active");
 		var index =$(this).index();
-		$("#main div").eq(index).show().siblings().hide();
+		$("#main .content").eq(index).show().siblings().hide();
 	})
 })
