a5ffdbdce7b8c36cb334e150475253c9c9c59e7e
ImmatureKid <1770125568@qq.com>
2019-04-06 15:11:46 +0800

    点就开始哭
    阿萨德

--- js/head.js
+++ js/head.js
@@ -1,21 +1,41 @@
 $(function(){
+	var index=0
 	$("#main .content").eq(0).show().siblings().hide();
-	$('.header-tap-ul li').click(function(){
-		console.log(1)
+	$('.header-tap-ul li').click(function(e){
+		// console.log(1)
 		$(this).addClass("active").siblings().removeClass("active");
-		var index =$(this).index();
+		 index =$(this).index();
 		var flag 
 		if (index==0||index==1 ) {
-			flag =true
-			if (flag) {
-			$("#bg .earth").addClass('animate').removeClass('animate')
-				flag = false
-			}else{
-				$("#bg .earth").removeClass('animate')
-				flag =true
-			}
+			$("#bg").show()
+			$("#bg .earth").toggleClass('animate');
+				
+			
+		}else{
+		$("#bg").hide();	
+		}
+		if (index==1 ) {
+		$("#bg div.yuanhome").addClass('xianshi');	
+		}else{
+			$("#bg div.yuanhome").removeClass('xianshi');
 		}
 		
 		$("#main .content").eq(index).show().siblings().hide();
+		if ($(e.target).attr("class")=="header-tap-item") {
+			console.log(1)
+			var index1 = $(e.target).index();
+			console.log($("#main .content").eq(index).children().eq(index1).show().siblings());
+			$("#main .content").eq(index).children().eq(index1+1).show().siblings().hide();
+		}
 	})
+	// function show(obj,obj1){
+	// 	obj.click(function(){
+	// 		var index1 =$(this).index();
+	// 		console.log(index1);
+	// 		obj1.eq(index1).show().siblings().hide();
+	// 		console.log($(".content:eq("+index+")>div").eq(index1));
+	// 	})
+
+	// }
+	// show($(".header-tap-ul li .head-tap-list a"),$(".content:eq("+index+")>div"));
 })
